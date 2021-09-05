6497c33546ba0a4c502bb820bbb5823c5a77c446
sakurakotubaki <73347485+sakurakotubaki@users.noreply.github.com>
2021-09-05 15:36:16 +0900

    Reactのmemoを使う 不要なサイレンダリングを防ぐ コンポーネントを囲って使う

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,20 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   hight: "200%",
   backgroundColor: "Khaki"
 };
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
   const { open } = props;
+  console.log("childAreaがレンダリングされた!!");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  console.log(data);
   return (
     <>
       {open ? (
@@ -15,4 +24,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
